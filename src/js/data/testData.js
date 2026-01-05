401a9df010739c4cb1078cad56fec9957a7d1e73
BNT266 <patrick.devosse@biontech.de>
2026-01-05 10:28:57 +0100

    Create testData.js

---
+++ src/js/data/testData.js
@@ -0,0 +1,42 @@
+// Test Data for Demo Mode
+export function getTestData() {
+    return `Land;Liegenschaft;Ereignisart;Datum
+Deutschland;Mainz Campus;Zutrittsverletzung;2025-01-03
+Deutschland;Mainz Campus;Zutrittsverletzung;2025-01-04
+Deutschland;Mainz Campus;Alarmanlage ausgelöst;2025-01-05
+Deutschland;Berlin Research;Zutrittsverletzung;2025-02-01
+Deutschland;Berlin Research;Verdächtige Person;2025-02-02
+Deutschland;Berlin Research;Verdächtige Person;2025-02-04
+Deutschland;München Warehouse;Diebstahl;2025-03-01
+Deutschland;München Warehouse;Diebstahl;2025-03-02
+Deutschland;München Warehouse;Alarmanlage ausgelöst;2025-03-05
+Deutschland;München Warehouse;Zutrittsverletzung;2025-03-06
+USA;Cambridge Lab;Zutrittsverletzung;2025-01-10
+USA;Cambridge Lab;Verdächtige Person;2025-01-12
+USA;Cambridge Lab;Alarmanlage ausgelöst;2025-01-15
+USA;San Diego Office;Verdächtige Person;2025-02-10
+USA;San Diego Office;Verdächtige Person;2025-02-12
+USA;San Diego Office;Diebstahl;2025-02-14
+USA;San Diego Office;Zutrittsverletzung;2025-02-16
+UK;London HQ;Zutrittsverletzung;2025-01-07
+UK;London HQ;Zutrittsverletzung;2025-01-09
+UK;London HQ;Verdächtige Person;2025-01-11
+UK;London HQ;Alarmanlage ausgelöst;2025-01-13
+UK;Reading Plant;Diebstahl;2025-03-03
+UK;Reading Plant;Diebstahl;2025-03-06
+UK;Reading Plant;Zutrittsverletzung;2025-03-07
+Schweiz;Basel Site;Verdächtige Person;2025-02-03
+Schweiz;Basel Site;Verdächtige Person;2025-02-05
+Schweiz;Basel Site;Alarmanlage ausgelöst;2025-02-06
+Schweiz;Basel Site;Zutrittsverletzung;2025-02-08
+Belgien;Brüssel Office;Zutrittsverletzung;2025-01-20
+Belgien;Brüssel Office;Diebstahl;2025-01-22
+Belgien;Brüssel Office;Diebstahl;2025-01-23
+Belgien;Brüssel Office;Verdächtige Person;2025-01-25
+Deutschland;Mainz Campus;Verdächtige Person;2025-01-06
+Deutschland;Mainz Campus;Diebstahl;2025-01-08
+USA;Cambridge Lab;Diebstahl;2025-01-18
+USA;San Diego Office;Alarmanlage ausgelöst;2025-02-20
+UK;Reading Plant;Alarmanlage ausgelöst;2025-03-08
+Schweiz;Basel Site;Diebstahl;2025-02-10`;
+}
